aa6d2b51f4046388fddb0ca3456d3708e49fd198
Claudiu <clau_123@yahoo.com>
2022-04-17 19:03:23 +0300

    working contract with normal economical rules

--- Blockchain/contracts/XCoinStaking.sol
+++ Blockchain/contracts/XCoinStaking.sol
@@ -9,45 +9,91 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract XCoinStaking is XCoin, Ownable{
   
-  uint256 percent = 10; 
+  uint256 stakeInterestRate = 7;
+  uint256 loanInterestRate = 12;
+  uint256 permittedExceeding = 14 days;
   address[] internal stakeholders;
   
+  struct Stake {
+    uint256 value;
+    uint256 monthlyReward;
+    uint256 placementDate;
+    uint256 nextClaim;
+  }
+  struct Loan {
+    uint256 value; //tokens
+    uint256 rate; //monthly
+    uint256 guaranty; //in wei
+    uint256 startDate; 
+    uint256 nextRate; 
+    uint256 ratesNumber;
+  }
+  
   mapping(address => uint256) public totalStakesPerUser;
-  mapping(address => uint256[]) public stakes;
+  mapping(address => Stake[]) public stakes;
+  mapping(address => Loan) public loans;
 
-  function convertDigitsToToken(uint256 value) public view returns(uint256) {
+  function convertTokensToDigits(uint256 value) public pure returns(uint256) {
     return value * (10**18);
-    // return value;
   }
 
-  function convertTokenToDigits(uint256 value) private view returns(uint256) {
+  function convertDigitsToTokens(uint256 value) public pure returns(uint256) {
     return value / (10**18);
+  }
+
+  function convertTimestamToDays(uint256 timestamp) public pure returns(uint256) {
+    return timestamp / 1 days;
+  }
+
+  function calculateGuaranty(uint256 loanValue, uint256 period) public pure returns(uint256) {
+    loanValue = convertTokensToDigits(loanValue);
+    return (loanValue / 100000) * period;
+  }
+
+  function calculateTotalPayment(uint256 loanValue, uint period) public view returns(uint256) {
+    // uint addings = (period - 1) * loanValue * loanInterestRate / 200;
+    return loanValue + period * loanValue * loanInterestRate / 100;
+  }
+
+  function calculateRate(uint256 totalPaymentValue, uint256 period) public pure returns(uint256) {
+    return totalPaymentValue / (period * 12);
+  }
+  
+  function calculateMonthlyReward(uint256 stakeValue) public view returns(uint256) {
+    return stakeValue * stakeInterestRate / 100;
+  }
+
+  //nu stiu cum se cheama persoana care face un imprumut
+  function penalizeUser(address user) internal{ 
+    uint256 difference = block.timestamp - loans[user].nextRate;
+
+    if (difference > permittedExceeding) {
+      if ((difference - permittedExceeding) / 1 days < 200) // daca depaseste 200 zile, nu ii mai dam garantia
+        //garantia -= nr de zile depasite peste perioada de 14 zile permisa, inmultit cu 0.5% din valoarea garantiei
+        loans[user].guaranty -= convertTimestamToDays(difference - permittedExceeding) * loans[user].guaranty / 200;
+      else
+        loans[user].guaranty = 0;
+    }
   }
   
   constructor() public {
-    uint256 balance = convertDigitsToToken(100000); 
+    uint256 balance = convertTokensToDigits(100000); 
     _mint(msg.sender, balance);
   }
-  
 
-  // receive() external payable {
-  //   _mint(msg.sender, msg.value * (10**10));
-  // }
-
-
-  function isStakeholder(address _address) private returns(bool, uint256) {
+  function isStakeholder(address _address) public view returns(bool, uint256) {
     for (uint256 i = 0; i < stakeholders.length; i += 1) {
       if (_address == stakeholders[i]) return (true, i);
     }
     return (false, 0);
   }
 
-  function addStakeholder(address _stakeholder) private {
+  function addStakeholder(address _stakeholder) public {
     (bool _isStakeholder, ) = isStakeholder(_stakeholder);
     if(!_isStakeholder) stakeholders.push(_stakeholder);
   }
 
-  function removeStakeholder(address _stakeholder) private {
+  function removeStakeholder(address _stakeholder) public {
     (bool _isStakeholder, uint256 i) = isStakeholder(_stakeholder);
     if(_isStakeholder) {
       stakeholders[i] = stakeholders[stakeholders.length - 1];
@@ -55,44 +101,112 @@ contract XCoinStaking is XCoin, Ownable{
     }
   }
 
-  function getMyStakesNumber() public view returns(uint256){
-    return stakes[msg.sender].length;
-  }
-
-  function getStakeValue(uint index) public view returns(uint256) {
-    return stakes[msg.sender][index];
-  }
-
-  function getMyStakes() public view returns (uint256[] memory) {
+  function getMyStakes() public view returns (Stake[] memory) {
     return stakes[msg.sender];
   }
   
   function stake(uint256 stakeValue) public {
-    stakeValue = convertDigitsToToken(stakeValue);
+    stakeValue = convertTokensToDigits(stakeValue); // 1 = 10**18
     approve(msg.sender, stakeValue);
     transferFrom(msg.sender, address(this), stakeValue);
 
     if(stakes[msg.sender].length == 0) addStakeholder(msg.sender);
-
-    stakes[msg.sender].push(stakeValue);
+    
+    stakes[msg.sender].push(
+      Stake(
+        stakeValue, 
+        calculateMonthlyReward(stakeValue),
+        block.timestamp, 
+        block.timestamp + 30 days
+      )
+    );
     totalStakesPerUser[msg.sender] += stakeValue;
   }
 
-  function withdrawStake(uint256 _stakeIndex) public {
-    uint256 stakeValue = stakes[msg.sender][_stakeIndex]; // salvam valoare
-    stakes[msg.sender][_stakeIndex] = stakes[msg.sender][stakes[msg.sender].length - 1]; //ii alocam valoarea de pe ultima pozitie
-    stakes[msg.sender].pop(); //eliminam ultimul element
-    if(stakes[msg.sender].length == 0) removeStakeholder(msg.sender);
-    transfer(msg.sender, stakeValue);
-    totalStakesPerUser[msg.sender] -= stakeValue;
+  function withdrawStake(uint256 stakeIndex) public {
+    require(stakes[msg.sender].length > stakeIndex, "Stake doesn't exist");
+    require(stakes[msg.sender][stakeIndex].placementDate + 360 days < block.timestamp, "Stake period didn't end yet");
+    require(stakes[msg.sender][stakeIndex].nextClaim > block.timestamp, "You have unclaimed rewards");
+    require(stakes[msg.sender][stakeIndex].value <= balanceOf(address(this)), "Sorry we are broke");
+
+    uint256 stakeValue = stakes[msg.sender][stakeIndex].value; 
+
+    //stergem stake-ul
+    stakes[msg.sender][stakeIndex] = stakes[msg.sender][stakes[msg.sender].length - 1]; 
+    stakes[msg.sender].pop(); 
     
+    if(stakes[msg.sender].length == 0) removeStakeholder(msg.sender);
+
+    
+    _mint(msg.sender, stakeValue);
+    _burn(address(this), stakeValue);
+
+    // transfer(msg.sender, stakeValue);
+    totalStakesPerUser[msg.sender] -= stakeValue;
   }
 
+  function claim(uint256 stakeIndex) public returns(bool){
+    require(stakes[msg.sender].length > stakeIndex, "Stake doesn't exist");
+    require(stakes[msg.sender][stakeIndex].nextClaim < block.timestamp, "You can claim once a mounth");
+    require(stakes[msg.sender][stakeIndex].monthlyReward <= balanceOf(address(this)), "Sorry we are broke");
 
+    _mint(msg.sender, stakes[msg.sender][stakeIndex].monthlyReward);
+    _burn(address(this), stakes[msg.sender][stakeIndex].monthlyReward);
 
-  function claim() public {
-    // _mint(msg.sender, reward);
+    // bool success = transfer(msg.sender, stakes[msg.sender][stakeIndex].monthlyReward);
+    // require(success, "can't transfer");
+    stakes[msg.sender][stakeIndex].nextClaim += 30 days;
+    // return success;
+  }
+
+  function borrow(uint256 loanValue, uint256 period) public payable {
+    uint256 guaranty = calculateGuaranty(loanValue, period); // 100.000 xCoin => 1 eth
+    uint256 loanValue = convertTokensToDigits(loanValue); // 1 xCoin => 10 ** 18 digits
+    uint256 paymentValue = calculateTotalPayment(loanValue, period); // adding the interest rate in digits
+    uint256 rate = calculateRate(paymentValue, period); //monthly rate in digits 
+    
+    require(uint256(msg.value) >= guaranty, "Not the correct guaranty");
+    require(loans[msg.sender].value == 0, "You can have only one loan at a time.");
+    require(loanValue <= balanceOf(address(this)), "We don't have enough tokens");
+    
+    _mint(msg.sender, loanValue);
+    _burn(address(this), loanValue);
+    
+    loans[msg.sender] = 
+      Loan(
+        paymentValue, //value
+        rate, //monthly rate
+        guaranty, //that eth guaranty
+        block.timestamp, //start date
+        block.timestamp + 30 days, //next rate date
+        period * 12 // number of rates
+      ); 
+  }
+
+  function payRate() public {
+    require(block.timestamp > loans[msg.sender].nextRate, "Too early for paying the rate");
+    require(loans[msg.sender].value != 0, "You dont have a loan");
+    
+    //plateste rata
+    approve(msg.sender, loans[msg.sender].rate);
+    transferFrom(msg.sender, address(this), loans[msg.sender].rate);
+
+    //in cazul in care a platit prea tarziu il penalizam
+    penalizeUser(msg.sender);
+
+    loans[msg.sender].ratesNumber -= 1;
+    //daca nu mai are rate de platit
+    if (loans[msg.sender].ratesNumber == 0) {
+      loans[msg.sender].value = 0;
+      //daca nu i s-a anulat de tot garantia
+      if (loans[msg.sender].guaranty > 0) {
+        //ii trimitem inapoi garantia
+        (bool success, ) = msg.sender.call{value: loans[msg.sender].guaranty}("");
+        require(success, "Failed to send Ether");
+      }
+    }
+    else {
+      loans[msg.sender].nextRate += 30 days;
+    }
   }
 }
-
-
--- Blockchain/test/xCoinStaking-test.js
+++ Blockchain/test/xCoinStaking-test.js
@@ -1,20 +1,60 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { time } = require("@openzeppelin/test-helpers");
+
+async function wait(days) {
+  await network.provider.send("evm_increaseTime", [86400 * days]);
+  await network.provider.send("evm_mine");
+}
 
 describe("xCoinStaking", function () {
   it("The contract is ok", async function () {
     const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
     const XCoinStaking = await ethers.getContractFactory("XCoinStaking");
     const xcoinStaking = await XCoinStaking.deploy();
-
     await xcoinStaking.deployed();
-    await xcoinStaking.stake(1);
-    await xcoinStaking.stake(2);
-    await xcoinStaking.stake(3);
-    await xcoinStaking.withdrawStake(0);
-    const value = await xcoinStaking.convertDigitsToToken(5);
-    expect(await xcoinStaking.totalStakesPerUser(ownerAddress)).to.equal(value);
+    await xcoinStaking.stake(10000);
+    expect(await xcoinStaking.balanceOf(xcoinStaking.address)).to.equals(
+      await xcoinStaking.convertTokensToDigits(10000)
+    );
+
+    await wait(30);
+    await xcoinStaking.claim(0);
+
+    expect(await xcoinStaking.balanceOf(ownerAddress)).to.equals(
+      await xcoinStaking.convertTokensToDigits(90700)
+    );
+
+    let guaranty = await xcoinStaking.calculateGuaranty(9300, 1);
+    await xcoinStaking.borrow(9300, 1, {
+      value: guaranty,
+    });
+
+    expect((await xcoinStaking.loans(ownerAddress)).guaranty).to.equals(
+      guaranty
+    );
+    // console.log(await xcoinStaking.loans(ownerAddress));
+
+    expect(await xcoinStaking.balanceOf(xcoinStaking.address)).to.equals(0);
+    for (let i = 1; i <= 12; i++) {
+      await wait(33);
+      await xcoinStaking.payRate();
+    }
+    // expect(await xcoinStaking.loans[ownerAddress].guaranty).to.equals(guaranty);
+    expect((await xcoinStaking.loans(ownerAddress)).guaranty).to.equals(
+      guaranty
+    );
+    guaranty = await xcoinStaking.calculateGuaranty(9300, 1);
+    await xcoinStaking.borrow(9300, 1, {
+      value: guaranty,
+    });
+
+    // expect(await xcoinStaking.balanceOf(xcoinStaking.address)).to.equals(0);
+    // expect(await xcoinStaking.claim(0)).to.equals(700);
+    // const value = await xcoinStaking.convertDigitsToTokens(5);
+    // expect(await xcoinStaking.totalStakesPerUser(ownerAddress)).to.equal(value);
+
     // const data = await xcoinStaking.getMyStakes();
-    // data.map((item) => console.log(parseInt(item._hex)));
+    // data.map((item) => console.log(parseInt(item.lastClaim._hex)));
   });
 });
